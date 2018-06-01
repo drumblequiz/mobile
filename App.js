2fe1b6accaeedb12ec018ad9712835c139c55f04
dbtst <jonasmotiejauskas@gmail.com>
2018-06-01 23:36:06 +0200

    new project added

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Blink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink182' />
+      </View>
+    );
+  }
+}
