a1dd4f0679efe88a0966bd947bada1efb0c1f5fc
Unknown <matasramanuaskas@gmail.com>
2018-06-21 20:53:54 +0200

    Corrected Buttons

--- screens/AnswerScreen.js
+++ screens/AnswerScreen.js
@@ -44,7 +44,7 @@ class AnswerScreen extends React.Component {
   handleChoiceClick(numb)
   {
     clearInterval(this.myInterval);
-    this.props.socket.answers.sort((a, b) => a.AnswerId < b.AnswerId);
+    //this.props.socket.answers.sort((a, b) => a.AnswerId < b.AnswerId);
     this.props.select(this.props.socket.answers[numb]);
     const resetAction = StackActions.reset({
       index: 0, actions: [
@@ -69,29 +69,33 @@ class AnswerScreen extends React.Component {
           <Text style={[{fontSize: 70, fontWeight: 'bold', textAlign: 'center'}]}>{this.getTimer()}</Text>
         </View>
         <View style={{flex:5, alignItems :'center', justifyContent:'center', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap',}}>
-          <TouchableOpacity style={[{width:140, height: 140}]} onPress={() => this.handleChoiceClick(1)}>
+          <TouchableOpacity style={[{width:140, height: 140}]} onPress={() => this.handleChoiceClick(0)}>
             <View marginTop={'3%'} marginBottom={'3%'} marginRight={'3%'} marginLeft={'3%'} style={[{width:'94%', height:'94%'}, theme.theme.playButton]}>
               <Image width={120} source={require('../images/elephant.png')}/>
+              <Text>{this.props.socket.answers[0].Answer}</Text>
             </View>
           </TouchableOpacity>
           { this.props.socket.answers.length > 1 ?
-          <TouchableOpacity style={[{width:140, height: 140}]} onPress={() => this.handleChoiceClick(2)}>
+          <TouchableOpacity style={[{width:140, height: 140}]} onPress={() => this.handleChoiceClick(1)}>
             <View marginTop={'3%'} marginBottom={'3%'} marginRight={'3%'} marginLeft={'3%'} style={[{width:'94%', height:'94%'}, theme.theme.playButton]}>
               <Image width={120} source={require('../images/lion.png')}/>
+              <Text>{this.props.socket.answers[1].Answer}</Text>
             </View>
           </TouchableOpacity>
           :<TouchableOpacity/>}
           {this.props.socket.answers.length > 2 ?
-          <TouchableOpacity style={[{width:140, height: 140}]} onPress={() => this.handleChoiceClick(3)}>
+          <TouchableOpacity style={[{width:140, height: 140}]} onPress={() => this.handleChoiceClick(2)}>
             <View marginTop={'3%'} marginBottom={'3%'} marginRight={'3%'} marginLeft={'3%'} style={[{width:'94%', height:'94%'}, theme.theme.playButton]}>
               <Image width={120} source={require('../images/crocodile.png')}/>
+              <Text>{this.props.socket.answers[2].Answer}</Text>
             </View>
           </TouchableOpacity>
           :<TouchableOpacity/>}
           {this.props.socket.answers.length > 3 ?
-          <TouchableOpacity style={[{width:140, height: 140}]} onPress={() => this.handleChoiceClick(4)}>
+          <TouchableOpacity style={[{width:140, height: 140}]} onPress={() => this.handleChoiceClick(3)}>
             <View marginTop={'3%'} marginBottom={'3%'} marginRight={'3%'} marginLeft={'3%'} style={[{width:'94%', height:'94%'}, theme.theme.playButton]}>
               <Image width={120} source={require('../images/flamingo.png')}/>
+              <Text>{this.props.socket.answers[3].Answer}</Text>
             </View>
           </TouchableOpacity>
           :<TouchableOpacity/>}
