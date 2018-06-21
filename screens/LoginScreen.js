7374b6f33ecfb14c4a89ce8208a7dbb0b38a3754
Unknown <jonasmotiejauskas@gmail.com>
2018-06-21 20:56:09 +0200

    fixed capitalize bug

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -82,7 +82,6 @@ class LoginScreen extends React.Component {
           <TextInput
             style={[styleGeneral.roomId, {width:300, height: 40}, theme.theme.element, theme.theme.textElement]}
             defaultValue=''
-            autoCapitalize={'characters'}
             underlineColorAndroid='transparent'
             maxLength={100}
             placeholderTextColor='black'
