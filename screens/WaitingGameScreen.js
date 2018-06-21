4fa59650b3e30ace2e7b2245c5be30ebc7d202a4
Unknown <matasramanuaskas@gmail.com>
2018-06-22 01:34:42 +0200

    Fixed lock in waitingForGame screen.

--- screens/WaitingGameScreen.js
+++ screens/WaitingGameScreen.js
@@ -3,12 +3,14 @@ import { AppRegistry, TouchableNativeFeedback, TextInput, View, Text} from 'reac
 import { SafeAreaView, StackActions, NavigationActions } from 'react-navigation';
 import Image from 'react-native-scalable-image';
 import { connect } from 'react-redux';
+import {showScoreStatusChanged, correctAnswerReceivedChanged, getPlayerRanking} from '../actions/network.js';
 
 import styleGeneral from '../styles/general.js';
 
 class WaitingGameScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.props.getPlayerRanking();
   }
 
   static navigationOptions = {
@@ -45,6 +47,16 @@ class WaitingGameScreen extends React.Component {
         });
         this.props.navigation.dispatch(resetAction);
     }
+    if(this.props.socket.showScoreStatus == "ok"){
+      this.props.showScoreStatusChanged("inactive");
+      this.props.correctAnswerReceivedChanged(false);
+      const resetAction = StackActions.reset({
+        index: 0, actions: [
+          NavigationActions.navigate({ routeName: 'Score' })
+        ],
+      });
+      this.props.navigation.dispatch(resetAction);
+    }
     return (
       <SafeAreaView style={{ backgroundColor: '#4FAFFF', flex:1, flexDirection: 'column', justifyContent:'center'}}>
         <View style={{flex:8, alignItems :'center', justifyContent:'center'}}>
@@ -63,4 +75,4 @@ const mapStateToProps = state => {
   return { language: state.language, socket: state.socket };
 };
 
-export default connect(mapStateToProps)(WaitingGameScreen);
+export default connect(mapStateToProps, {showScoreStatusChanged, correctAnswerReceivedChanged, getPlayerRanking, })(WaitingGameScreen);
